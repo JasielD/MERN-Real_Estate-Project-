d63c9eb304d41f4eab5fc7632bf336a708e37652
JasielD <dsilvajasiel@gmail.com>
2025-09-19 12:50:38 +0530

    create get user listings api route

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser,getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const UserRouter = express.Router();
@@ -7,5 +7,6 @@ const UserRouter = express.Router();
 UserRouter.get("/", test );
 UserRouter.put("/update/:id", verifyToken, updateUser)
 UserRouter.delete("/delete/:id", verifyToken, deleteUser)
+UserRouter.get("/listings/:id",verifyToken, getUserListings)
 
 export default UserRouter;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcrypt from "bcrypt";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
     res.json({ message: "User route is working!!!" });
@@ -40,4 +41,19 @@ export const deleteUser = async(req,res,next)=>{
    } catch (err) {
     next(err);
    }
+}
+
+export const getUserListings = async (req,res,next)=>{
+    if(req.user.id === req.params.id){
+    try {
+            const listings = await Listing.find({userRef:req.params.id});
+            res.status(200).json(listings);
+            
+        } catch (err) {
+            next(err)
+        }
+    }else{
+        return next(errorHandler(401,"You can only view your own listings"))
+    } 
+    
 }
