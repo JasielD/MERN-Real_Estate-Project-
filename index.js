93a254654e578c7699616820f43664c821a7993c
JasielD <dsilvajasiel@gmail.com>
2025-09-27 18:04:35 +0530

    f this

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ app.use("/api/auth",AuthRouter);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use("/api/listing",ListingRouter);
 
-// app.use(express.static(path.join(__dirname,"/client/dist")))
+app.use(express.static(path.join(__dirname,"/client/dist")))
 
-// app.get("*",(req,res)=>{
-//   res.sendFile(path.join(__dirname,"/client/dist/index.html"))
-// })
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"/client/dist/index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
